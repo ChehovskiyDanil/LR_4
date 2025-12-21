ae68cc0ad4b322ba30d4d52cdd92750d97147a81
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-12-21 17:46:03 +0300

    Add to individ2.7.py shebang and edits

--- individ2_7.py
+++ individ2_7.py
@@ -1,30 +1,29 @@
-mlist = []
-c = 10
-for i in range(c):
-    mlist.append(float(input(i)))
-print('Начальный список = ',mlist)
-print('Минимальный элемент = ',min(mlist),' его индекс = ',mlist.index(min(mlist)))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-minimal = []
-for i in mlist:
-    if i < 0:
-        minimal.append(mlist.index(i))
-print(minimal)
-for i in minimal:
-    if minimal.index(i) != 0 and minimal.index(i) != 1:
-        minimal.remove(i)
-minimal.pop()
-start = minimal[0]
-end = minimal[1]
-print('Промежуток = ',minimal,' сумма данного промежутка = ',sum(mlist[start+1:end]))
+if __name__ == '__main__':
+    mlist = []
+    c = 10
+    for i in range(c):
+        mlist.append(float(input(i)))
+    print('Начальный список = ',mlist)
+    print('Минимальный элемент = ',min(mlist),' его индекс = ',mlist.index(min(mlist)))
 
-abs1=[]
-for i in mlist:
-    if abs(i) <= 1:
-        abs1.append(i)
-other = []
-for i in mlist:
-    if abs(i) > 1:
-        other.append(i)
-result = abs1 + other
-print('Преобразованный список = ', result)
+    minimal = []
+    for i in mlist:
+        if i < 0:
+            minimal.append(mlist.index(i))
+    start = minimal[0]
+    end = minimal[1]
+    print('Промежуток = ',mlist[start:end+1],' сумма данного промежутка = ',sum(mlist[start+1:end]))
+
+    abs1=[]
+    for i in mlist:
+        if abs(i) <= 1:
+            abs1.append(i)
+    other = []
+    for i in mlist:
+        if abs(i) > 1:
+            other.append(i)
+    result = abs1 + other
+    print('Преобразованный список = ', result)
