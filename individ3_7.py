0967ab1179dfe12dd46c8242ecde4ddbf70bb763
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-12-21 19:17:18 +0300

    Add to individ3.7.py shebang

--- individ3_7.py
+++ individ3_7.py
@@ -1,15 +1,19 @@
-Mytuple = (1, 2, 4, 3, 6, 8, 5)
-print(Mytuple)
-last = -1
-for i in range(len(Mytuple) - 1):
-    if Mytuple[i] % 2 == 0 and Mytuple[i + 1] % 2 == 0:
-        last = i
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-if last != -1:
-    result = Mytuple[:last]
-    if result:
-        print("Элементы, предшествующие последней паре соседних чётных чисел:",*result)
+if __name__ == '__main__':
+    Mytuple = (1, 2, 4, 3, 6, 8, 5)
+    print(Mytuple)
+    last = -1
+    for i in range(len(Mytuple)):
+        if Mytuple[i] % 2 == 0 and Mytuple[i + 1] % 2 == 0:
+            last = i
+
+    if last != -1:
+        result = Mytuple[:last]
+        if result:
+            print("Элементы, предшествующие последней паре соседних чётных чисел:",*result)
+        else:
+            print("Перед последней парой нет элементов.")
     else:
-        print("Перед последней парой нет элементов.")
-else:
-    print("В массиве нет пары соседних чётных чисел.")
+        print("В массиве нет пары соседних чётных чисел.")
