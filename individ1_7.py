47ff926e8c429c1e6051253a09b43babe3bd1e4c
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-12-21 17:43:56 +0300

    Add shebang

--- individ1_7.py
+++ individ1_7.py
@@ -1,11 +1,15 @@
-a = []
-b = 10
-for i in range(b):
-    a.append(int(input(i)))
-print(a)
-c = []
-for i in a:
-    if i < 0:
-        c.append(i)
-print(c)
-print(sum(c))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    a = []
+    b = 10
+    for i in range(b):
+        a.append(int(input(i)))
+    print(a)
+    c = []
+    for i in a:
+        if i < 0:
+            c.append(i)
+    print(c)
+    print(sum(c))
